71cf6af099b22279f66659ca4496203bf00bd2c3
monzter50 <josemonzter50@gmail.com>
2022-03-02 15:10:17 -0500

    add event swip

--- src/components/MoodItemRow.tsx
+++ src/components/MoodItemRow.tsx
@@ -7,34 +7,82 @@ import {
   LayoutAnimation,
 } from 'react-native';
 import { format } from 'date-fns';
+import {
+  PanGestureHandler,
+  // PanGestureHandlerGestureEvent,
+} from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+  useAnimatedGestureHandler,
+  runOnJS,
+} from 'react-native-reanimated';
 import { MoodOptionWithTimestamp } from '../types';
 import { theme } from '../theme';
 import { useAppContext } from '../Providers/App.provider';
 
+const maxPan = 80;
 export const MoodItemRow: React.FC<MoodOptionWithTimestamp> = ({
   mood,
   timestamp,
 }) => {
   const appContext = useAppContext();
+  const offset = useSharedValue(0);
+
   const onDelete = useCallback((): void => {
     LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
     appContext.handleDelete(timestamp);
   }, [appContext, timestamp]);
+
+  const removeWithDelay = React.useCallback(() => {
+    setTimeout(() => {
+      LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+      appContext.handleDelete(timestamp);
+    }, 250);
+  }, [appContext, timestamp]);
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ translateX: offset.value }],
+  }));
+  const onGestureEvent = useAnimatedGestureHandler(
+    {
+      onActive: event => {
+        console.warn(event.translationX);
+        offset.value = event.translationX;
+      },
+      onEnd: event => {
+        if (Math.abs(event.translationX) > maxPan) {
+          // if the item should be removed, animate it off the screen first
+          offset.value = withTiming(Math.sign(offset.value) * 2000);
+
+          // then trigger the remove mood item with a small delay
+          runOnJS(removeWithDelay)();
+        } else {
+          // otherwise, animate the item back to the start
+          offset.value = withTiming(0);
+        }
+      },
+    },
+    [],
+  );
+
   return (
-    <View key={timestamp} style={styles.moodItem}>
-      <View style={styles.moodHeader}>
-        <Text style={styles.moodValue}>{mood.emoji}</Text>
-        <Text style={styles.moodTitle}>{mood.description}</Text>
-      </View>
-      <View>
-        <Text style={styles.moodDate}>
-          {format(new Date(timestamp), "d MMM, yyyy 'at' h:mmaaa")}
-        </Text>
-      </View>
-      <Pressable onPress={onDelete}>
-        <Text>Delete</Text>
-      </Pressable>
-    </View>
+    <PanGestureHandler onGestureEvent={onGestureEvent}>
+      <Animated.View key={timestamp} style={[styles.moodItem, animatedStyle]}>
+        <View style={styles.moodHeader}>
+          <Text style={styles.moodValue}>{mood.emoji}</Text>
+          <Text style={styles.moodTitle}>{mood.description}</Text>
+        </View>
+        <View>
+          <Text style={styles.moodDate}>
+            {format(new Date(timestamp), "d MMM, yyyy 'at' h:mmaaa")}
+          </Text>
+        </View>
+        <Pressable onPress={onDelete}>
+          <Text>Delete</Text>
+        </Pressable>
+      </Animated.View>
+    </PanGestureHandler>
   );
 };
 
