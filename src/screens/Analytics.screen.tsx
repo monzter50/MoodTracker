15ab49f845b04f73bf8fc7e20ae1f00b8a2bb51f
monzter50 <josemonzter50@gmail.com>
2022-03-03 09:31:16 -0500

    add pie char and loadash depencies

--- src/screens/Analytics.screen.tsx
+++ src/screens/Analytics.screen.tsx
@@ -1,10 +1,33 @@
+import { groupBy } from 'lodash';
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-
+import { StyleSheet, View } from 'react-native';
+import { VictoryPie } from 'victory-native';
+import { useAppContext } from '../Providers/App.provider';
+import { theme } from '../theme';
 export const AnalyticsScreen: React.FC = () => {
+  const appContext = useAppContext();
+  const data = Object.entries(
+    groupBy(appContext.selectedMoods, 'mood.emoji'),
+  ).map(([key, value]) => ({
+    x: key,
+    y: value.length,
+  }));
   return (
     <View style={styles.container}>
-      <Text>AnalyticsScreen</Text>
+      <VictoryPie
+        labelRadius={80}
+        radius={150}
+        innerRadius={50}
+        colorScale={[
+          theme.colorPurple,
+          theme.colorLavender,
+          theme.colorBlue,
+          theme.colorGrey,
+          theme.colorWhite,
+        ]}
+        style={{ labels: { fontSize: 30 } }}
+        data={data}
+      />
     </View>
   );
 };
@@ -12,5 +35,7 @@ export const AnalyticsScreen: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
--- src/theme.tsx
+++ src/theme.tsx
@@ -3,6 +3,9 @@ export const theme = {
   primaryColor: '#4B5DE8',
   colorLavender: '#87677B',
   colorGrey: '#8E9AAF',
+  colorWhite: '#fff',
+  colorBlue: '#4B5DE8',
+  colorPurple: '#454C73',
 
   fontFamilyBold: 'Kalam-Bold',
   fontFamilyRegular: 'Kalam-Regular',
